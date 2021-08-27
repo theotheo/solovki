7679234c4730de4084aeb4ffbf49efc684d0f597
i <ibelyalov@yandex.ru>
2021-08-27 21:43:04 +0300

    Dynamic change video

--- ar-helpers.js
+++ ar-helpers.js
@@ -1,16 +1,36 @@
 AFRAME.registerComponent('videohandler', {
     init: function() {
         var marker = this.el;
-        this.vid = document.querySelector("#vid");
+        // this.vid = document.querySelector("#vid");
+        // console.log(this.vid)
 
         marker.addEventListener('markerFound', function() {
+            console.log('markerFound')
+
             this.toggle = true;
-            this.vid.play();
+
+            let vidEl = document.querySelector("#vid")
+            console.log(vidEl)
+            let id = vidEl.dataset.video
+                // console.log(vid)
+                // el.play()
+            let assetEl = document.querySelector(`${id}`)
+            console.log(assetEl)
+            assetEl.play();
         }.bind(this));
 
         marker.addEventListener('markerLost', function() {
             this.toggle = false;
-            this.vid.pause();
+            console.log('markerLost')
+
+            let vidEl = document.querySelector("#vid")
+            console.log(vidEl)
+            let id = vidEl.dataset.video
+                // console.log(vid)
+                // el.play()
+            let assetEl = document.querySelector(`${id}`)
+            console.log(assetEl)
+            assetEl.pause();
         }.bind(this));
     },
 });
